89a6fcb92342b2c56f70ead57cee3e392ad3b78a
Wataru Matsui <wmatsui@zlab.co.jp>
2019-07-22 14:30:07 +0900

    Improved test

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ fn run(args: Vec<String>) -> Result<String> {
         Command::Build { build, ver } => ver.update_build(build),
     };
 
-    debug!("{:?}", &version);
+    debug!("version: {:?}", &version);
     Ok(version.to_string())
 }
 
@@ -53,43 +53,37 @@ fn main() {
 mod tests {
     use super::*;
 
+    fn test_run(row_args: Vec<&str>, expect: &str) {
+        let args = row_args.into_iter().map(String::from).collect();
+
+        let actual = run(args);
+        assert_eq!(actual.unwrap(), String::from(expect));
+    }
+
     #[test]
     fn inc_patch() {
         let version = "0.1.0";
         let expect = "0.1.1";
-        let args = vec!["bump", "patch", version]
-            .into_iter()
-            .map(String::from)
-            .collect();
-
-        let actual = run(args);
-        assert_eq!(actual.unwrap(), String::from(expect));
+        let args = vec!["bump", "patch", version];
+        test_run(args, expect);
     }
 
     #[test]
     fn inc_minor() {
         let version = "0.1.1";
         let expect = "0.2.0";
-        let args = vec!["bump", "minor", version]
-            .into_iter()
-            .map(String::from)
-            .collect();
-
-        let actual = run(args);
-        assert_eq!(actual.unwrap(), String::from(expect));
+        let args = vec!["bump", "minor", version];
+        test_run(args, expect);
     }
 
     #[test]
     fn inc_major() {
         let version = "0.1.1";
         let expect = "1.0.0";
-        let args = vec!["bump", "major", version]
-            .into_iter()
-            .map(String::from)
-            .collect();
+        let args = vec!["bump", "major", version];
+        test_run(args, expect);
+    }
 
-        let actual = run(args);
-        assert_eq!(actual.unwrap(), String::from(expect));
     }
 
     #[test]
@@ -97,13 +91,8 @@ mod tests {
         let version = "1.0.0";
         let pre = "alpha.0";
         let expect = "1.0.0-alpha.0";
-        let args = vec!["bump", "pre", pre, version]
-            .into_iter()
-            .map(String::from)
-            .collect();
-
-        let actual = run(args);
-        assert_eq!(actual.unwrap(), String::from(expect));
+        let args = vec!["bump", "pre", pre, version];
+        test_run(args, expect);
     }
 
     #[test]
@@ -111,26 +100,44 @@ mod tests {
         let version = "1.0.0-alpha.0";
         let pre = "beta.0";
         let expect = "1.0.0-beta.0";
-        let args = vec!["bump", "pre", pre, version]
-            .into_iter()
-            .map(String::from)
-            .collect();
-
-        let actual = run(args);
-        assert_eq!(actual.unwrap(), String::from(expect));
+        let args = vec!["bump", "pre", pre, version];
+        test_run(args, expect);
     }
 
     #[test]
-    fn ignore_ver_prefix() {
+    fn remove_build() {
+        let version = "1.0.0+20190722";
+        let expect = "1.0.1";
+        let args = vec!["bump", "patch", version];
+        test_run(args, expect);
+    }
+
+    #[test]
+    fn add_build() {
+        let version = "1.0.0";
+        let build = "20190722";
+        let expect = "1.0.0+20190722";
+        let args = vec!["bump", "build", build, version];
+        test_run(args, expect);
+    }
+
+    #[test]
+    fn replace_build() {
+        let version = "1.0.0+20190722";
+        let build = "20190723";
+        let expect = "1.0.0+20190723";
+        let args = vec!["bump", "build", build, version];
+        test_run(args, expect);
+    }
+
+    #[test]
+    fn ignore_default_prefix() {
         let version = "v1.0.0";
         let expect = "v2.0.0";
-        let args = vec!["bump", "major", version]
-            .into_iter()
-            .map(String::from)
-            .collect();
+        let args = vec!["bump", "major", version];
+        test_run(args, expect);
+    }
 
-        let actual = run(args);
-        assert_eq!(actual.unwrap(), String::from(expect));
     }
 
 }
